f6fe558d887d9a0c342bb116ad3c4e99e5b9f63d
ksm30289 <ksm30289@gmail.com>
2026-06-24 14:48:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from crawler.dcinside import get_dc_posts
 from crawler.floor import get_floor_posts
 
-dc_posts = get_dc_posts()
-floor_posts = get_floor_posts()
 
 print("===== DC =====")
 
-for post in dc_posts:
+dc_posts = get_dc_posts()
+
+for post in dc_posts[:5]:
     print(post)
 
+print()
 print("===== FLOOR =====")
 
-for post in floor_posts:
+floor_posts = get_floor_posts()
+
+for post in floor_posts[:5]:
     print(post)
