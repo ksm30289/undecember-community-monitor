7b2c909a9f281738fa6a6093ba6ed4f60f7c1b5a
ksm30289 <ksm30289@gmail.com>
2026-06-24 15:42:24 +0900

    Update floor.py

--- crawler/floor.py
+++ crawler/floor.py
@@ -33,7 +33,15 @@ def get_floor_posts():
         "a.all-link.bbs-detail-link"
     )
 
-    for row in rows:
+   for row in rows:
+
+        data_type = row.get("data-type")
+
+        # 공지 제외
+        if data_type == "icon_notice":
+            continue
+
+        post_no = row.get("data-no")
 
         article_id = row.get("article-id")
 
